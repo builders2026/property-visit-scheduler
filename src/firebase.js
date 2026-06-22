1328039b50e1f51cf50746f1b35b694c1e13c634
builders2026 <freecreditbyharsh@gmail.com>
2026-06-23 02:40:10 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,8 +7,8 @@ const firebaseConfig = {
   projectId: "property-visits-ee21f",
   storageBucket: "property-visits-ee21f.firebasestorage.app",
   messagingSenderId: "733225973484",
-  appId: "1:733225973484:web:1136010121910350b7d7c9",
-  measurementId: "G-WSWDLWVJEZ"
+  appId: "1:733225973484:web:9d78c4c9356fc9a9b7d7c9",
+  measurementId: "G-D0VYDQ3DTQ"
 };
 
 const app = initializeApp(firebaseConfig);
